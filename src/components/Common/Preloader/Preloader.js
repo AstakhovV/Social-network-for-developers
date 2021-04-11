50f6672960c24a3792e0f6aa722a3b02466e8a4c
AstakhovV <astakhclever@gmail.com>
2021-04-11 21:03:03 +0500

    a lot of modification and refactoring

---
+++ src/components/Common/Preloader/Preloader.js
@@ -0,0 +1,10 @@
+import loader from "../../../assets/image/loader.gif";
+import React from "react";
+
+let Preloader = () => {
+    return <div>
+        <img src={loader}/>
+    </div>
+
+}
+export default Preloader;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,26 +1,95 @@
 import React from "react";
-import Users from "./Users";
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
+import {
+    follow,
+    setCurrentPage,
+    toogleIsFetching,
+    setUsers,
+    setUsersTotalCount,
+    unfollow
+} from "../../redux/users-reducer";
+import * as axios from "axios";
+import Users from "./Users";
+import Preloader from "../Common/Preloader/Preloader";
+
+class UsersContainer extends React.Component { // классовая компонента юзерсАПИ
+    constructor(props) {
+        super(props);
+    } // конструктор можно не писать, если в нем больше ничего нет
+    //  как в данном случае. pagination - постраничный вывод
+
+    componentDidMount(): void {// ajax-запрос на сервер с помощью библиотеки axios
+        this.props.toogleIsFetching(true)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize} `)
+            .then(response => {
+                this.props.toogleIsFetching(false)
+                this.props.setUsers(response.data.items)
+                this.props.setUsersTotalCount(response.data.totalCount) //запрос количество пользователей с сервера
+            })
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.toogleIsFetching(true)
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize} `)
+            .then(response => {
+                this.props.toogleIsFetching(false)
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    render() {
+        return <>
+            {this.props.isFetching ? <Preloader/> : null}
+            <Users totalUsersCount={this.props.totalUsersCount}
+                   pageSize={this.props.pageSize}
+                   currentPage={this.props.currentPage}
+                   users={this.props.users}
+                   onPageChanged={this.onPageChanged}
+                   unfollow={this.props.unfollow}
+                   follow={this.props.follow}
+
+            />
+        </>
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage,
+        isFetching: state.usersPage.isFetching
     }
 }
 
-let mapDispatchToProps = (dispatch) => {
-    return {
-        follow: (userId) => {
-            dispatch (followAC(userId))
-        },
-        unfollow: (userId) => {
-            dispatch (unfollowAC(userId))
-        },
-        setUsers: (users) => {
-            dispatch (setUsersAC(users))
-        }
-    }
-}
+// let mapDispatchToProps = (dispatch) => {
+//     return {
+//         follow: (userId) => {
+//             dispatch(followAC(userId))
+//         },
+//         unfollow: (userId) => {
+//             dispatch(unfollowAC(userId))
+//         },
+//         setUsers: (users) => {
+//             dispatch(setUsersAC(users))
+//         },
+//         setCurrentPage: (pageNumber) => {
+//             dispatch(setCurrentPageAC(pageNumber))
+//         },
+//         setTotalUsersCount: (totalCount) => {
+//             dispatch(setUsersTotalCountAC(totalCount))
+//         },
+//         toggleIsFetching: (isFetching) => {
+//             dispatch(setIsFetchingAC(isFetching))
+//         }
+//
+//     }
+// } e
+// dispatch оригинал, ниже укороченная версия
 
-export default connect (mapStateToProps, mapDispatchToProps) (Users);
+export default connect(mapStateToProps, {
+        follow, unfollow, setUsers, setCurrentPage, setUsersTotalCount, toogleIsFetching
+    }
+)(UsersContainer);
