1423286e52495227f239093fc8066a4c2e9fd30f
AstakhovV <astakhclever@gmail.com>
2021-05-28 14:18:40 +0500

    Tests for components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import './index.css';
-import store from "./redux/redux-store";
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import MainApp from "./App";
 
 ReactDOM.render(
-    <BrowserRouter>
-    <Provider store={store}>
-        <App //state={store.getState()} dispatch={store.dispatch.bind(store)} store={store}
-        />
-    </Provider>
-    </BrowserRouter>, // провайдер для прокидывания пропсов в компоненты
+    <MainApp />, // провайдер для прокидывания пропсов в компоненты
     document.getElementById('root')
 )
 
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
@@ -19,7 +19,10 @@ let reducers = combineReducers({
 })// склейка reducer в один объект
 //applyMiddleware - функция для редакса для работы thunk
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+));
 
 window.store = store;
 
